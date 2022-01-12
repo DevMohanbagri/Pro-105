5af522d0313fe520c295323b6a0195f4f675e4f0
DevMohanbagri <73389635+DevMohanbagri@users.noreply.github.com>
2022-01-12 18:57:36 +0530

    Update pro-105.py

--- pro-105.py
+++ pro-105.py
@@ -4,26 +4,26 @@ with open('../csv/stdev-105.csv', newline='') as f:
     reader = csv.reader(f)
     file_data = list(reader)
 
-#If I don't type this it shows error
-data = file_data.pop(0)
-print(data)
 
-n = 0
-def  mean(dataset):
-    n = len(dataset)
-    total = 0
-    for x in dataset:
-         total += float(x[1])
     
-    mean = total/n
-    return mean
+data = file_data.pop(0)
+
+
+
+n = len(data)
+total = 0
+for x in data:
+    total += int(x[1])
+    print(x)
+mean = total/n
+print(mean)
 
 # Squaring and getting the values
 squared_list = []
+print(data)
 
-
-for no in data:
-    a = int(no)-mean(data)
+for x in data:
+    a = x-mean(data)
     a = a**2
     squared_list.append(a)
 
